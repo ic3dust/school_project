92252c554ff5df3f6062ddfbecfd2a5e3097f48c
worker1.4kurs <o.step@kitu.nau>
2025-09-23 16:22:26 +0200

    name popup

--- checkbox.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('showpass').addEventListener('change', function(){
-    const password = document.getElementById('password');
-    password.type= this.checked ? 'text' : 'password';
-});
